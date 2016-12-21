c27e3abe6bedc9e843d647af01a3eb51a5a7a22f
ziv-zh <mayzzw@126.com>
2016-12-21 14:49:57 +0800

    add sample

---
+++ SpringContainer/src/main/java/com/abc/viewcontainer/springcontainer/Drag2LoadListener.java
@@ -0,0 +1,9 @@
+package com.abc.viewcontainer.springcontainer;
+
+/**
+ * Created by hzzhangzhenwei on 2016/12/21.
+ */
+
+public interface Drag2LoadListener {
+    void load(SpringContainer v);
+}
---
+++ SpringContainer/src/main/java/com/abc/viewcontainer/springcontainer/Pull2RefreshListener.java
@@ -0,0 +1,9 @@
+package com.abc.viewcontainer.springcontainer;
+
+/**
+ * Created by hzzhangzhenwei on 2016/12/21.
+ */
+
+public interface Pull2RefreshListener {
+    void onRefresh(SpringContainer v);
+}
